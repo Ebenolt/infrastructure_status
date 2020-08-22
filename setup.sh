f8caa9c9df8da10e5913c208f3c2329aac5aab7d
Ebenolt <me@ebenolt.com>
2020-08-22 20:02:51 +0200

    Pretty setup

--- setup.sh
+++ setup.sh
@@ -10,12 +10,21 @@ clear
 
 username=$(who am i | awk '{print $1}')
 working_dir=$(pwd)
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
-apt-get update
-apt-get install php7.7 php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-mysql php7.4-zip php7.4-fpm php7.4-mbstring
-apt-get install nginx python3 curl mysql-client python3-pip mysql-server apt-transport-https lsb-release ca-certificates && pip3 install mysql-connector
 
-systemctl enable mariadb.service nginx
+
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+apt-get update >> /dev/null 2>&1
+printf "Installing webserver, sql server & utilities: "
+apt-get install nginx python3 curl mysql-client python3-pip mysql-server apt-transport-https lsb-release ca-certificates -y >> /dev/null 2>&1 && printf "\e[32mOK\e[0m" || printf "\e[31mError\e[0m"
+
+printf "\n Installing PHP: "
+apt-get install php7.7 php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-mysql php7.4-zip php7.4-fpm php7.4-mbstring -y >> /dev/null 2>&1 && printf "\e[32mOK\e[0m" || printf "\e[31mError\e[0m"
+
+printf "\n Installing pip mysql-connector: "
+pip3 install mysql-connector >> /dev/null 2>&1 && printf "\e[32mOK\e[0m" || printf "\e[31mError\e[0m"
+printf "\n\n"
+
+systemctl enable mariadb.service nginx >> /dev/null 2>&1
 
 echo "Database infos:"
 read -p " -Database IP: " host_ip
